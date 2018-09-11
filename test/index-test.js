6fbe4f6394f3bfa3347f4faaf695e656a3beb81a
carlo montanaro <carloldn@outlook.com>
2018-09-11 19:39:22 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -6,7 +6,7 @@ beforeEach(function() {
 })
 
 it('add(a, b) adds two numbers and returns the result', function() {
-  expect(add(a, b)). learn.toEqual(a + b)
+  expect(add(a, b)).toEqual(a + b)
 })
 
 it('subtract(a, b) subtracts b from a and returns the result', function() {
