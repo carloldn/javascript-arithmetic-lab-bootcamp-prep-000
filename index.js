a695801bf44cfa607b1838c8fe51a2a99fa35070
carlo montanaro <carloldn@outlook.com>
2018-09-11 19:21:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 function add () {
-  number+=81
+ a+b
 }
  
-function divide() {
-  number /= 3
+function subtract() {
+  a-b
 }
  
-divideBy3()
- 
-console.log(number) // 3.333333333335
- 
-add5()
- 
-console.log(number) // 8.333333333335
- 
-// reset number
-number = 10
- 
-add5()
- 
-console.log(number) // 15
- 
-divideBy3()
- 
-console.log(number) // 5
